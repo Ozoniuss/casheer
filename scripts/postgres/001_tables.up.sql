7e201c21a935df1dc8214534ccdcd65426af05f2
ozoniuss <alexbledea_2000@yahoo.com>
2023-03-08 00:12:17 +0200

    feat(): add database tables and local docker setup

---
+++ scripts/postgres/001_tables.up.sql
@@ -0,0 +1,38 @@
+--todo: add indexes
+
+CREATE TABLE IF NOT EXISTS entries(
+    id UUID NOT NULL PRIMARY KEY DEFAULT gen_random_uuid(),
+    month SMALLINT NOT NULL,
+    year SMALLINT NOT NULL,
+    category TEXT NOT NULL,
+    subcategory TEXT NOT NULL,
+    expected_total REAL NOT NULL,
+    running_total REAL NOT NULL,
+    recurring BOOLEAN NOT NULL DEFAULT FALSE,
+    created_at TIMESTAMPTZ,
+    updated_at TIMESTAMPTZ,
+    deleted_at TIMESTAMPTZ,
+    CONSTRAINT unique_logical_entry UNIQUE (month, year, category, subcategory)
+);
+
+CREATE TABLE IF NOT EXISTS expenses(
+    id UUID NOT NULL PRIMARY KEY DEFAULT gen_random_uuid(),
+    entry_id UUID NOT NULL REFERENCES entries(uuid) ON DELETE CASCADE,
+    value REAL NOT NULL,
+    name TEXT NOT NULL,
+    description TEXT,
+    payment_method TEXT,
+    created_at TIMESTAMPTZ,
+    updated_at TIMESTAMPTZ,
+    deleted_at TIMESTAMPTZ,
+);
+
+CREATE TABLE IF NOT EXISTS debts(
+    id UUID NOT NULL PRIMARY KEY DEFAULT gen_random_uuid(),
+    person TEXT NOT NULL,
+    amount REAL NOT NULL,
+    details TEXT,
+    created_at TIMESTAMPTZ,
+    updated_at TIMESTAMPTZ,
+    deleted_at TIMESTAMPTZ,
+);
