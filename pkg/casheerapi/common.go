70f45e42471e3f8fe5a3c1cf9bc0bb31c9c515fe
ozoniuss <alexbledea_2000@yahoo.com>
2023-03-08 02:20:00 +0200

    feat(pkg): add common error message

--- pkg/casheerapi/common.go
+++ pkg/casheerapi/common.go
@@ -11,3 +11,17 @@ type Timestamps struct {
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 }
+
+// Error represents an error message that is sent to the client in case the
+// HTTP request returns an error.
+//
+// The error message is modelled after the jsonapi specification:
+// https://jsonapi.org/format/#error-objects. Many of the suggested fields had
+// not been included, since they were unnecessary for the size of this project.
+// In production, however, it is generally a good idea to stick to a
+// well-defined standard.
+type Error struct {
+	Status int    `json:"status"`
+	Title  string `json:"title"`
+	Detail string `json:"detail"`
+}
