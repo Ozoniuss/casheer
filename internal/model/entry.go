c4c702dba94d23f2a82dc85bca14cdfb8a63b44c
ozoniuss <alexbledea_2000@yahoo.com>
2023-03-12 15:47:13 +0200

    feat(model): add database delete hook for entry

--- internal/model/entry.go
+++ internal/model/entry.go
@@ -1,5 +1,10 @@
 package model
 
+import (
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
 // Entry models an entry of a planning. Fields are automatically mapped by gorm
 // to their database equivalents.
 type Entry struct {
@@ -13,4 +18,11 @@ type Entry struct {
 	ExpectedTotal float32
 	RunningTotal  float32
 	Recurring     bool
+}
+
+func (e *Entry) AfterDelete(tx *gorm.DB) (err error) {
+	if e.Id == uuid.Nil {
+		err = gorm.ErrRecordNotFound
+	}
+	return
 }
