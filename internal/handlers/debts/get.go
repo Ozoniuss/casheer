bc05161104d436f610bc101175cd0ccd30e22b60
ozoniuss <alexbledea_2000@yahoo.com>
2023-03-12 17:17:37 +0200

    feat(handlers): get debt

---
+++ internal/handlers/debts/get.go
@@ -0,0 +1,51 @@
+package debts
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+
+	"github.com/Ozoniuss/casheer/internal/handlers/common"
+	"github.com/Ozoniuss/casheer/internal/model"
+	"github.com/Ozoniuss/casheer/pkg/casheerapi"
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+func (h *handler) HandleGetDebt(ctx *gin.Context) {
+
+	id := ctx.Param("id")
+	uuid, err := uuid.Parse(id)
+	if err != nil {
+		common.EmitError(ctx, NewGetDebtFailed(
+			http.StatusBadRequest,
+			fmt.Sprintf("Could not update debt: invalid uuid format: %s", id),
+		))
+		return
+	}
+
+	var debt model.Debt
+	err = h.db.WithContext(ctx).Where("id = ?", uuid).Take(&debt).Error
+
+	if err != nil {
+		switch {
+		case errors.Is(err, gorm.ErrRecordNotFound):
+			common.EmitError(ctx, NewGetDebtFailed(
+				http.StatusNotFound,
+				fmt.Sprintf("Could not retrieve Debt: debt %s not found.", uuid)))
+			return
+		default:
+			common.EmitError(ctx, NewGetDebtFailed(
+				http.StatusInternalServerError,
+				fmt.Sprintf("Could not retrieve debt: %s", err.Error())))
+			return
+		}
+	}
+
+	resp := casheerapi.GetDebtResponse{
+		Data: DebtToPublic(debt),
+	}
+
+	ctx.JSON(http.StatusOK, &resp)
+}
