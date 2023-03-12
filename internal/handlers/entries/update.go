ce3f5b70d685a7648bcf87d4cb6240b5dcac5cd5
ozoniuss <alexbledea_2000@yahoo.com>
2023-03-12 14:52:55 +0200

    feat(handlers): update entry

---
+++ internal/handlers/entries/update.go
@@ -0,0 +1,75 @@
+package entries
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/Ozoniuss/casheer/internal/handlers/common"
+	"github.com/Ozoniuss/casheer/internal/model"
+	"github.com/Ozoniuss/casheer/pkg/casheerapi"
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+	"gorm.io/gorm/clause"
+)
+
+func (h *handler) HandleUpdateEntry(ctx *gin.Context) {
+
+	id := ctx.Param("id")
+	uuid, err := uuid.Parse(id)
+	if err != nil {
+		common.EmitError(ctx, NewDeleteEntryFailedError(
+			http.StatusBadRequest,
+			fmt.Sprintf("Could not update entry: invalid uuid format: %s", id),
+		))
+	}
+
+	var req casheerapi.UpdateEntryRequest
+	err = ctx.BindJSON(&req)
+
+	if err != nil {
+		common.EmitError(ctx, NewUpdateEntryFailed(
+			http.StatusBadRequest,
+			fmt.Sprintf("Could not bind request body: %s", err.Error())))
+		return
+	}
+
+	// This is needed to query using zero values as well, see
+	// https://gorm.io/docs/update.html#Updates-multiple-columns
+	var updatedFields map[string]any
+
+	if req.Month != nil {
+		updatedFields["month"] = int8(*req.Month)
+	}
+	if req.Year != nil {
+		updatedFields["year"] = int16(*req.Year)
+	}
+	if req.Category != nil {
+		updatedFields["category"] = *req.Category
+	}
+	if req.Subcategory != nil {
+		updatedFields["subcategory"] = *req.Subcategory
+	}
+	if req.Recurring != nil {
+		updatedFields["recurring"] = *req.Recurring
+	}
+	if req.ExpectedTotal != nil {
+		updatedFields["expected_total"] = *req.ExpectedTotal
+	}
+
+	var entry model.Entry
+	err = h.db.Clauses(clause.Returning{}).Where("id = ?", uuid).Updates(req).Error
+
+	// TODO: nicer error handling
+	if err != nil {
+		common.EmitError(ctx, NewUpdateEntryFailed(
+			http.StatusInternalServerError,
+			fmt.Sprintf("Could not update entry: %s", err.Error())))
+		return
+	}
+
+	resp := casheerapi.UpdateEntryResponse{
+		Data: EntryToPublic(entry),
+	}
+
+	ctx.JSON(http.StatusOK, &resp)
+}
