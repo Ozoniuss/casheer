f3e6682f03a0f4416d6eb314498f81d8550b9ff3
ozoniuss <alexbledea_2000@yahoo.com>
2023-03-12 14:53:09 +0200

    feat(handlers): list entries

---
+++ internal/handlers/entries/list.go
@@ -0,0 +1,60 @@
+package entries
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/Ozoniuss/casheer/internal/handlers/common"
+	"github.com/Ozoniuss/casheer/internal/model"
+	"github.com/Ozoniuss/casheer/pkg/casheerapi"
+	"github.com/gin-gonic/gin"
+)
+
+func (h *handler) HandleListEntry(ctx *gin.Context) {
+
+	var params casheerapi.ListEntryParams
+	err := ctx.BindQuery(&params)
+
+	if err != nil {
+		common.EmitError(ctx, NewListEntryFailedError(
+			http.StatusBadRequest,
+			fmt.Sprintf("Could not bind query params: %s", err.Error())))
+		return
+	}
+
+	// This is needed to query using zero values as well, see
+	// https://gorm.io/docs/query.html#Struct-amp-Map-Conditions
+	var filters map[string]any
+
+	if params.Month != nil {
+		filters["month"] = int8(*params.Month)
+	}
+	if params.Year != nil {
+		filters["year"] = int16(*params.Year)
+	}
+	if params.Category != nil {
+		filters["category"] = *params.Category
+	}
+	if params.Subcategory != nil {
+		filters["subcategory"] = *params.Subcategory
+	}
+
+	var entries []model.Entry
+	err = h.db.Where(filters).Order("year desc").Order("month desc").Find(&entries).Error
+
+	// TODO: nicer error handling
+	if err != nil {
+		common.EmitError(ctx, NewListEntryFailedError(
+			http.StatusInternalServerError,
+			fmt.Sprintf("Could not list entries: %s", err.Error())))
+		return
+	}
+
+	resp := casheerapi.ListEntryResponse{
+		Data: make([]casheerapi.EntryData, 0, len(entries)),
+	}
+	for _, e := range entries {
+		resp.Data = append(resp.Data, EntryToPublic(e))
+	}
+	ctx.JSON(http.StatusCreated, &resp)
+}
