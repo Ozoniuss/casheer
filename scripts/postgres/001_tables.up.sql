c9fb8e12deecfef3c4875bee9d0fee333da7d89f
ozoniuss <alexbledea_2000@yahoo.com>
2023-03-31 03:16:56 +0300

    fix(scripts): expense foreign key

--- scripts/postgres/001_tables.up.sql
+++ scripts/postgres/001_tables.up.sql
@@ -17,14 +17,15 @@ CREATE TABLE IF NOT EXISTS entries(
 
 CREATE TABLE IF NOT EXISTS expenses(
     id UUID NOT NULL PRIMARY KEY DEFAULT gen_random_uuid(),
-    entry_id UUID NOT NULL REFERENCES entries(id) ON DELETE CASCADE,
+    entry_id UUID NOT NULL,
     value REAL NOT NULL,
     name TEXT NOT NULL,
     description TEXT,
     payment_method TEXT,
     created_at TIMESTAMPTZ NOT NULL,
     updated_at TIMESTAMPTZ NOT NULL,
-    deleted_at TIMESTAMPTZ
+    deleted_at TIMESTAMPTZ,
+    CONSTRAINT entry_key FOREIGN KEY(entry_id) REFERENCES entries(id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS debts(
