f829b3c791bb35f39090458638d9f6688368fb8d
Ozoniuss <qscxzsewqazxcde@gmail.com>
2023-05-01 00:50:18 +0300

    feat(store): configure database settings

--- internal/store/connect.go
+++ internal/store/connect.go
@@ -2,6 +2,7 @@ package store
 
 import (
 	"fmt"
+	"time"
 
 	cfg "github.com/Ozoniuss/casheer/internal/config"
 
@@ -18,6 +19,36 @@ func getDSN(config cfg.Database) string {
 
 // Connect establishes a connection to the postgres database, using the
 // connectivity information provided in the config.
+//
+// Note that managing the connection pool from go and disabling the database's
+// built in pooling system (e.g. pgpool) might give better performance. This is
+// not really relevant in my case though.
 func Connect(config cfg.Database) (*gorm.DB, error) {
-	return gorm.Open(postgres.Open(getDSN(config)), &gorm.Config{})
+	session, err := gorm.Open(postgres.New(postgres.Config{
+		DSN:                  getDSN(config),
+		PreferSimpleProtocol: false, // enables prepared statements (should be active by default)
+		WithoutReturning:     false,
+	}), &gorm.Config{})
+
+	if err != nil {
+		// Note that open doesn't connect to the database, first connection is
+		// established when first interacting with it.
+		return nil, fmt.Errorf("could not open database: %w", err)
+	}
+
+	db, err := session.DB()
+	if err != nil {
+		return nil, fmt.Errorf("could not retrieve connection pool: %w", err)
+	}
+
+	// A session of inserting expenses should not take more than one hour
+	// TODO: check how fast the database itself closes an idle connection.
+	db.SetConnMaxLifetime(time.Hour)
+
+	// Since I will be the only one using the app I could even lower these
+	// to 1...
+	db.SetMaxIdleConns(2)
+	db.SetMaxOpenConns(2)
+
+	return session, nil
 }
