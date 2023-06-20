24de7b7a633dcc7c9e6654d70ee1deb1a031887a
ozoniuss <alexbledea_2000@yahoo.com>
2023-06-20 23:58:52 +0300

    chore(model): remove unused function

--- internal/model/entry_test.go
+++ internal/model/entry_test.go
@@ -1,26 +1,10 @@
 package model
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/go-playground/validator/v10"
-	"github.com/google/uuid"
 )
-
-// genUuidPattern generates an uuid based on the provided pattern. It is assumed
-// that the pattern has lenght 1,2 or 4.
-func genUuidPattern(pattern string) uuid.UUID {
-	if len(pattern) != 1 && len(pattern) != 2 && len(pattern) != 4 {
-		panic(fmt.Sprintf("invalid pattern length %d", len(pattern)))
-	}
-	if len(pattern) == 1 {
-		pattern = fmt.Sprintf("%[1]s%[1]s%[1]s%[1]s", pattern)
-	} else if len(pattern) == 2 {
-		pattern = fmt.Sprintf("%[1]s%[1]s", pattern)
-	}
-	return uuid.MustParse(fmt.Sprintf("%[1]s%[1]s-%[1]s-%[1]s-%[1]s-%[1]s%[1]s%[1]s", pattern))
-}
 
 func TestValidEntry(t *testing.T) {
 	tests := []struct {
