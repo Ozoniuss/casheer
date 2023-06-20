56ec4cf7f411a7e9abb7e018583a0b2998aa0f07
ozoniuss <alexbledea_2000@yahoo.com>
2023-06-20 16:09:59 +0300

    feat(): custom middleware for binding request body

--- internal/handlers/common/error.go
+++ internal/handlers/common/error.go
@@ -1,6 +1,9 @@
 package common
 
 import (
+	"fmt"
+
+	ierrors "github.com/Ozoniuss/casheer/internal/errors"
 	"github.com/Ozoniuss/casheer/pkg/casheerapi"
 	public "github.com/Ozoniuss/casheer/pkg/casheerapi"
 	"github.com/gin-gonic/gin"
@@ -14,4 +17,25 @@ func EmitError(ctx *gin.Context, err public.Error) {
 	ctx.JSON(err.Status, casheerapi.ErrorResponse{
 		Err: err,
 	})
+}
+
+// CtxGetTypes is very similar to ctx.Get(), except it makes use of generics.
+func CtxGetTyped[T casheerapi.CreateEntryRequest](ctx *gin.Context, param string) (T, bool) {
+	reqval, ok := ctx.Get(param)
+	if !ok {
+		EmitError(ctx, ierrors.NewMissingContextParamError(
+			fmt.Sprintf("Context parameter %s not found.", param),
+		))
+		return T{}, false
+	}
+
+	req, ok := reqval.(T)
+	if !ok {
+		EmitError(ctx, ierrors.NewInvalidContextParamError(
+			fmt.Sprintf("Context parameter %s has invalid type.", param),
+		))
+		return T{}, false
+	}
+
+	return req, true
 }
--- internal/middlewares/middlewares.go
+++ internal/middlewares/middlewares.go
@@ -1,10 +1,31 @@
 package middlewares
 
 import (
+	"fmt"
+
 	ierrors "github.com/Ozoniuss/casheer/internal/errors"
 	"github.com/Ozoniuss/casheer/internal/handlers/common"
+	"github.com/Ozoniuss/casheer/pkg/casheerapi"
 	"github.com/gin-gonic/gin"
 )
+
+// BindJSONRequest does the same job as ctx.ShouldBindJSON, while also writing
+// a custom error to the response if the binding is not succesful.
+func BindJSONRequest[T casheerapi.CreateEntryRequest]() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		var req T
+		err := ctx.ShouldBindJSON(&req)
+		if err != nil {
+			common.EmitError(ctx, ierrors.NewRequestBindingError(
+				fmt.Sprintf("Could not bind request body: %s", err.Error()),
+			))
+			ctx.Abort()
+			return
+		}
+		ctx.Set("req", req)
+		ctx.Next()
+	}
+}
 
 func RequiredParam[T int](paramName string) gin.HandlerFunc {
 	return func(ctx *gin.Context) {
