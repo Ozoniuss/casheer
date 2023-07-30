db31a621c0346f24d6eafc046d1911c5f6ee8b84
qscxzsewqazxcde@gmail.com <qscxzsewqazxcde@gmail.com>
2023-07-30 17:23:33 +0300

    feat(sqlite3): use COLLATE NOCASE for TEXT fields

--- scripts/sqlite/001_tables.up.sql
+++ scripts/sqlite/001_tables.up.sql
@@ -10,8 +10,8 @@ CREATE TABLE IF NOT EXISTS entries(
     id INTEGER PRIMARY KEY,
     month INTEGER NOT NULL, -- smallint?
     year INTEGER NOT NULL,
-    category TEXT NOT NULL,
-    subcategory TEXT NOT NULL,
+    category TEXT NOT NULL COLLATE NOCASE,
+    subcategory TEXT NOT NULL COLLATE NOCASE,
     -- Even though these values are not necessarily integers, I only care about
     -- two digits of precision, so I will use "bani" or "cents" as the lowest
     -- unit of precis   ion. I'm still debating between dollar, euro and leu, but
@@ -29,9 +29,9 @@ CREATE TABLE IF NOT EXISTS expenses(
     id INTEGER PRIMARY KEY,
     entry_id INTEGER NOT NULL,
     value INTEGER NOT NULL,
-    name TEXT NOT NULL,
-    description TEXT,
-    payment_method TEXT,
+    name TEXT NOT NULL COLLATE NOCASE,
+    description TEXT COLLATE NOCASE,
+    payment_method TEXT COLLATE NOCASE,
     paid_at DATETIME, -- sometimes I might find it useful to know when the expense was paid at
     created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -41,9 +41,9 @@ CREATE TABLE IF NOT EXISTS expenses(
 
 CREATE TABLE IF NOT EXISTS debts(
     id INTEGER PRIMARY KEY,
-    person TEXT NOT NULL,
+    person TEXT NOT NULL COLLATE NOCASE,
     amount INTEGER NOT NULL,
-    details TEXT,
+    details TEXT COLLATE NOCASE,
     created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     deleted_at DATETIME, -- safety net
