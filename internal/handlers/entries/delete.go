c6034068cc2e9c816b26ddf51afdf86e1e5eccac
qscxzsewqazxcde@gmail.com <qscxzsewqazxcde@gmail.com>
2023-08-09 21:25:44 +0300

    refactor(handlers): use middlewares when deleting entries

--- internal/handlers/entries/delete.go
+++ internal/handlers/entries/delete.go
@@ -1,15 +1,12 @@
 package entries
 
 import (
-	"errors"
-	"fmt"
 	"net/http"
 
 	"github.com/Ozoniuss/casheer/internal/handlers/common"
 	"github.com/Ozoniuss/casheer/internal/model"
 	"github.com/Ozoniuss/casheer/pkg/casheerapi"
 	"github.com/gin-gonic/gin"
-	"gorm.io/gorm"
 	"gorm.io/gorm/clause"
 )
 
@@ -17,32 +14,18 @@ func (h *handler) HandleDeleteEntry(ctx *gin.Context) {
 
 	id := ctx.GetInt("entid")
 
-	entry := model.Entry{}
+	var entry model.Entry
 	err := h.db.WithContext(ctx).Clauses(clause.Returning{}).Preload("expenses").Where("id = ?", id).Delete(&entry).Error
 
 	if err != nil {
-		switch {
-		case errors.Is(err, gorm.ErrRecordNotFound):
-			common.EmitError(ctx, NewDeleteEntryFailedError(
-				http.StatusNotFound,
-				fmt.Sprintf("Could not delete entry: entry %d not found.", id)))
-			return
-		default:
-			common.EmitError(ctx, NewDeleteEntryFailedError(
-				http.StatusInternalServerError,
-				fmt.Sprintf("Could not delete entry: %s", err.Error())))
-			return
-		}
+		common.ErrorAndAbort(ctx, err)
+		return
 	}
 
 	resp := casheerapi.CreateEntryResponse{
 		Data: EntryToPublic(entry, h.entriesURL, computeRunningTotal(entry.Expenses)),
 	}
-
-	// The following resources were removed, thus the links should be empty as
-	// well.
 	resp.Data.Links.Self = ""
-	// resp.Data.Links.Expenses = ""
 
 	ctx.JSON(http.StatusOK, &resp)
 }
