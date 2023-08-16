2dd93512b880e4aaa5d468c6510d8562dd4b28f4
qscxzsewqazxcde@gmail.com <qscxzsewqazxcde@gmail.com>
2023-08-16 23:54:58 +0300

    refactor(entries): workaround for cascading soft-deletes

--- internal/handlers/entries/delete.go
+++ internal/handlers/entries/delete.go
@@ -7,6 +7,7 @@ import (
 	"github.com/Ozoniuss/casheer/internal/model"
 	"github.com/Ozoniuss/casheer/pkg/casheerapi"
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 	"gorm.io/gorm/clause"
 )
 
@@ -15,8 +16,31 @@ func (h *handler) HandleDeleteEntry(ctx *gin.Context) {
 	id := ctx.GetInt("entid")
 
 	var entry model.Entry
-	err := h.db.WithContext(ctx).Clauses(clause.Returning{}).Preload("expenses").Where("id = ?", id).Delete(&entry).Error
+	var expenses []model.Expense
 
+	// workaround because cascading soft deletes doesn't fucking work
+	err := h.db.WithContext(ctx).Transaction(func(tx *gorm.DB) error {
+
+		txerr := h.db.Where("entry_id = ?", id).Find(&expenses).Error
+		if txerr != nil {
+			return txerr
+		}
+
+		txerr = h.db.Clauses(clause.Returning{}).Where("id = ?", id).Delete(&entry).Error
+		if txerr != nil {
+			return txerr
+		}
+		// Preload doesn't fucking work either, but having the expenses is
+		// required in order to produce the output resource.
+		entry.Expenses = expenses
+
+		txerr = h.db.Delete(&expenses).Error
+		if txerr != nil {
+			return txerr
+		}
+
+		return nil
+	})
 	if err != nil {
 		common.ErrorAndAbort(ctx, err)
 		return
