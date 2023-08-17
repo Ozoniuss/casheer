010f45411cf4cd00f61ea67a477e3b3df34ecd4c
qscxzsewqazxcde@gmail.com <qscxzsewqazxcde@gmail.com>
2023-08-17 23:59:39 +0300

    refactor(debts): proper update debt validation and error handling

--- internal/handlers/debts/update.go
+++ internal/handlers/debts/update.go
@@ -19,30 +19,38 @@ func (h *handler) HandleUpdateDebt(ctx *gin.Context) {
 		return
 	}
 
-	// This is needed to query using zero values as well, see
-	// https://gorm.io/docs/update.html#Updates-multiple-columns
-	var updatedFields = make(map[string]any)
+	updatedDebt, fields := getUpdatedFields(req)
 
-	if req.Person != nil {
-		updatedFields["person"] = *req.Person
-	}
-	if req.Amount != nil {
-		updatedFields["amount"] = *req.Amount
-	}
-	if req.Details != nil {
-		updatedFields["details"] = *req.Details
-	}
-
-	var Debt model.Debt
-	err = h.db.WithContext(ctx).Model(&Debt).Clauses(clause.Returning{}).Where("id = ?", id).Updates(updatedFields).Error
+	err = h.db.WithContext(ctx).Select(fields).Clauses(clause.Returning{}).Scopes(model.ValidateModelScope[model.Debt](updatedDebt)).Where("id = ?", id).Updates(&updatedDebt).Error
 	if err != nil {
 		common.ErrorAndAbort(ctx, err)
 		return
 	}
 
 	resp := casheerapi.UpdateDebtResponse{
-		Data: DebtToPublic(Debt, h.debtsURL),
+		Data: DebtToPublic(updatedDebt, h.debtsURL),
 	}
 
 	ctx.JSON(http.StatusOK, &resp)
+}
+
+func getUpdatedFields(req casheerapi.UpdateDebtRequest) (model.Debt, []string) {
+
+	debt := model.Debt{}
+	var updatedFields = make([]string, 0, 3)
+
+	if req.Person != nil {
+		updatedFields = append(updatedFields, "person")
+		debt.Person = *req.Person
+	}
+	if req.Amount != nil {
+		updatedFields = append(updatedFields, "amount")
+		debt.Amount = *req.Amount
+	}
+	if req.Details != nil {
+		updatedFields = append(updatedFields, "details")
+		debt.Details = *req.Details
+	}
+
+	return debt, updatedFields
 }
