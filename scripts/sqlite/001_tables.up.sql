e6b6192d8da0401b60060bff3531a6056203293e
qscxzsewqazxcde@gmail.com <qscxzsewqazxcde@gmail.com>
2023-08-26 20:13:04 +0300

    chore(scripts): make currency name collate

--- scripts/sqlite/001_tables.up.sql
+++ scripts/sqlite/001_tables.up.sql
@@ -29,7 +29,7 @@ CREATE TABLE IF NOT EXISTS expenses(
     id INTEGER PRIMARY KEY,
     entry_id INTEGER NOT NULL,
     amount INTEGER NOT NULL,
-    currency TEXT NOT NULL, -- iso 4217 currency code
+    currency TEXT NOT NULL COLLATE NOCASE, -- iso 4217 currency code
     exponent INTEGER NOT NULL,
     name TEXT NOT NULL COLLATE NOCASE,
     description TEXT COLLATE NOCASE,
