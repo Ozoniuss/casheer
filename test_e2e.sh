e1b43206f090712f6e41c8964e12ed7643187f43
qscxzsewqazxcde@gmail.com <qscxzsewqazxcde@gmail.com>
2023-10-30 10:46:53 +0200

    feat: allow custom database name

--- test_e2e.sh
+++ test_e2e.sh
@@ -1,6 +1,14 @@
-sqlite3 casheer.e2e.db < scripts/sqlite/002_update_entry_value.down.sql && 
-sqlite3 casheer.e2e.db < scripts/sqlite/001_tables.down.sql && 
-sqlite3 casheer.e2e.db < scripts/sqlite/001_tables.up.sql &&
-sqlite3 casheer.e2e.db < scripts/sqlite/002_update_entry_value.up.sql &&
+DBNAME='casheer.e2e.db'
+
+if [[ -n $1 ]]; then
+    DBNAME="$1"
+fi 
+
+echo "Using database $DBNAME"
+
+sqlite3 "$DBNAME" < scripts/sqlite/002_update_entry_value.down.sql && 
+sqlite3 "$DBNAME" < scripts/sqlite/001_tables.down.sql && 
+sqlite3 "$DBNAME" < scripts/sqlite/001_tables.up.sql &&
+sqlite3 "$DBNAME" < scripts/sqlite/002_update_entry_value.up.sql &&
 go test -v ./e2e/entries_test.go ; 
 echo 'done';
