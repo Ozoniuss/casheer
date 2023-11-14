150645572223d097f9b5b6cd21c966817df86ab5
qscxzsewqazxcde@gmail.com <qscxzsewqazxcde@gmail.com>
2023-11-14 23:38:46 +0200

    feat: add new entry validation

--- internal/model/entry.go
+++ internal/model/entry.go
@@ -6,8 +6,6 @@ import (
 
 // Entry models an entry of a planning. Fields are automatically mapped by gorm
 // to their database equivalents.
-//
-// TODO: custom validation message.
 type Entry struct {
 	BaseModel
 	Value
@@ -19,6 +17,23 @@ type Entry struct {
 	Recurring   bool
 
 	Expenses []Expense
+}
+
+func (e Entry) Validate() error {
+	b := NewBaseModelErrorBuilder("entry")
+	if e.Month > 12 || e.Month < 1 {
+		b.AddError("month must be between 1 and 12")
+	}
+	if e.Year < 2020 {
+		b.AddError("year must be at least 2020")
+	}
+	if len(e.Category) == 0 {
+		b.AddError("category cannot be empty")
+	}
+	if len(e.Subcategory) == 0 {
+		b.AddError("subcategory cannot be empty")
+	}
+	return b.Error()
 }
 
 type InvalidEntryErr struct {
--- internal/model/entry_test.go
+++ internal/model/entry_test.go
@@ -99,3 +99,69 @@ func TestValidEntry(t *testing.T) {
 		}
 	}
 }
+
+func TestValidEntryNewValidation(t *testing.T) {
+	tests := []struct {
+		name  string
+		entry Entry
+		valid bool
+	}{
+		{
+			name: "valid entry",
+			entry: Entry{
+				Month:       10,
+				Year:        2022,
+				Category:    "one",
+				Subcategory: "two",
+			},
+			valid: true,
+		},
+		{
+			name: "invalid month",
+			entry: Entry{
+				Month:       13,
+				Year:        2022,
+				Category:    "one",
+				Subcategory: "two",
+			},
+			valid: false,
+		},
+		{
+			name: "invalid year",
+			entry: Entry{
+				Month:       11,
+				Year:        2019,
+				Category:    "one",
+				Subcategory: "two",
+			},
+			valid: false,
+		},
+		{
+			name: "invalid category",
+			entry: Entry{
+				Month:       11,
+				Year:        -15,
+				Category:    "",
+				Subcategory: "two",
+			},
+			valid: false,
+		},
+		{
+			name: "invalid subcategory",
+			entry: Entry{
+				Month:       11,
+				Year:        -15,
+				Category:    "one",
+				Subcategory: "",
+			},
+			valid: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if (tt.entry.Validate() == nil) != tt.valid {
+				t.Fatalf("expected to be valid: %t, got validation error: %v", tt.valid, tt.entry.Validate())
+			}
+		})
+	}
+}
