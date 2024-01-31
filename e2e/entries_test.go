cf7af3599bad234e4855e0d3195c4a0063b51530
Ozoniuss <qscxzsewqazxcde@gmail.com>
2024-01-31 22:41:14 +0200

    Remove some commented code

--- e2e/entries_test.go
+++ e2e/entries_test.go
@@ -63,42 +63,6 @@ func TestCreateEntryWithExpensesFlow(t *testing.T) {
 	if err != nil {
 		t.Fatalf("Did not expect error when creating expense; got %s\n", err.Error())
 	}
-
-	// getResp, err := casheerDebtClient.GetDebt(did)
-	// if err != nil {
-	// 	t.Error("Expected the returned debt Id to point to a valid debt, got error instead.")
-	// }
-
-	// if createResp.Data != getResp.Data {
-	// 	t.Errorf("Debt returned in GET request should match debt returned from POST request. Got %+v, want %+v", createResp.Data, getResp.Data)
-	// }
-
-	// deleteResp, err := casheerDebtClient.DeleteDebt(did)
-	// if err != nil {
-	// 	t.Error("Expected to be able to delete the newly created debt.")
-	// }
-
-	// if deleteResp.Data.Attributes != getResp.Data.Attributes {
-	// 	t.Errorf("Attributes of debt returned in DELETE request should match debt returned from POST request. Got %+v, want %+v", createResp.Data, getResp.Data)
-	// }
-	// if deleteResp.Data.Links.Self != "" {
-	// 	t.Errorf("Expected no self link, got %s\n", deleteResp.Data.Links.Self)
-	// }
-	// t.Logf("Deleted debt with id %d\n", did)
-
-	// _, err = casheerDebtClient.GetDebt(did)
-	// if err == nil {
-	// 	t.Fatal("Expected to get an error when retrieving deleted debt, got none.")
-	// }
-
-	// jsonerr, ok := err.(casheerapi.ErrorResponse)
-	// if !ok {
-	// 	t.Fatalf("Expected to get error of type jsonapi, got %v\n", err)
-	// }
-
-	// if jsonerr.Err.Status != 404 {
-	// 	t.Errorf("Expected to get status 404, got %d\n", jsonerr.Err.Status)
-	// }
 }
 
 func TestCreateListFilterEntryFlow(t *testing.T) {
