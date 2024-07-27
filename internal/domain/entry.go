83cb3a7bd9df2e6fa1672a59ace7e7070d3339e7
Ozoniuss <qscxzsewqazxcde@gmail.com>
2024-07-27 20:11:23 +0300

    Add expense events
    This change defines and emits change events whenever the expenses
    associated with a given entry are subject to change.

--- internal/domain/entry.go
+++ internal/domain/entry.go
@@ -2,12 +2,17 @@ package domain
 
 import (
 	"errors"
+	"slices"
 	"time"
 
 	"github.com/Ozoniuss/casheer/internal/domain/currency"
 )
 
 // Entry models an entry that can have zero or more expenses.
+//
+// An Entry, together with all Expenses, is an aggregate root, given that
+// Entries and Expenses have to be strongly consistent. Changes to an Expense
+// only happen via an Entry's public methods.
 //
 // Do not modify an entry directly. Use the provided functions to ensure
 // entries are always valid.
@@ -22,6 +27,10 @@ type Entry struct {
 	Recurring   bool
 
 	Expenses []Expense
+
+	// Stores all changes that happened to this entry's expenses. One example
+	// use case of this is database optimizations.
+	ExpensesChanged []ExpenseChangedEvent
 }
 
 func NewEntry(month int, year int, category, subcategory string, recurring bool) (Entry, error) {
@@ -58,7 +67,51 @@ func NewEntry(month int, year int, category, subcategory string, recurring bool)
 
 func (e *Entry) AddExpense(exp Expense) {
 	e.Expenses = append(e.Expenses, exp)
+	e.ExpensesChanged = append(e.ExpensesChanged, ExpenseChangedEvent{
+		Data:   exp,
+		Status: ExpenseCreated,
+	})
 }
+
+func (e *Entry) ModifyExpense(exp Expense) error {
+	for idx := range e.Expenses {
+		if e.Expenses[idx].Id == exp.Id {
+			e.Expenses[idx] = exp
+			e.ExpensesChanged = append(e.ExpensesChanged, ExpenseChangedEvent{
+				Data:   exp,
+				Status: ExpenseModified,
+			})
+			return nil
+		}
+	}
+	return ErrMissingExpense
+}
+
+func (e *Entry) DeleteExpense(exp Expense) error {
+	pos := slices.IndexFunc(e.Expenses, func(expense Expense) bool {
+		return expense.Id == exp.Id
+	})
+	if pos == -1 {
+		return ErrMissingExpense
+	}
+	e.Expenses = slices.Delete(e.Expenses, pos, pos+1)
+	return nil
+}
+
+type ExpenseChangedEvent struct {
+	Data   Expense
+	Status ExpenseStatus
+}
+
+type ExpenseStatus string
+
+const (
+	ExpenseCreated  ExpenseStatus = "created"
+	ExpenseModified ExpenseStatus = "modified"
+	ExpenseDeleted  ExpenseStatus = "deleted"
+)
 
 var ErrEmptyCategory = errors.New("category must not be empty")
 var ErrEmptySubcategory = errors.New("subcategory must not be empty")
+
+var ErrMissingExpense = errors.New("the expense is not part of the aggregate")
