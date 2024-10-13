4838ac6e1ddbc5e7a9a176e04b92b11be0d1418a
Ozoniuss <qscxzsewqazxcde@gmail.com>
2024-10-13 17:07:03 +0300

    Fix existing script for adding default entries

--- scripts/fill-expenses/create_default_expenses.go
+++ scripts/fill-expenses/create_default_expenses.go
@@ -2,52 +2,58 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"os"
 
 	"github.com/Ozoniuss/casheer/client/httpclient"
 )
 
-type data struct {
-	category       string
-	subcategory    string
-	name           string
-	description    string
-	amount         int
-	currency       string
-	payment_method string
-}
-
 type ExpData struct {
 	ExpectedTotal float32 `json:"expected_total"`
 	Recurring     bool    `json:"recurring"`
+	Currency      string  `json:"currency"`
 }
 
-func main() {
-	f, _ := os.Open("default.json")
+func run() error {
+	f, err := os.Open("../../default.json")
+	if err != nil {
+		return fmt.Errorf("could not open file: %s", err.Error())
+	}
 	defer f.Close()
 
 	var data = make(map[string]map[string]ExpData)
 
 	dec := json.NewDecoder(f)
-	err := dec.Decode(&data)
+	err = dec.Decode(&data)
 	if err != nil {
-		panic(err)
+		return fmt.Errorf("could not decode: %s", err.Error())
 	}
 
 	cclient, err := httpclient.NewCasheerHTTPClient(
 		httpclient.WithCustomAuthority("localhost:8033"),
 	)
 	if err != nil {
-		panic(err)
+		return fmt.Errorf("could not create client: %s", err.Error())
 	}
 
 	for category, subcategories := range data {
 		for subcategory, data := range subcategories {
-			_, err := cclient.CreateEntry(9, 2023, category, subcategory, int(data.ExpectedTotal*100), data.Recurring)
+			if data.Currency == "" {
+				data.Currency = "RON"
+			}
+			_, err := cclient.CreateEntry(10, 2024, category, subcategory, int(data.ExpectedTotal*100), data.Currency, data.Recurring)
 			if err != nil {
-				panic(err)
+				fmt.Printf("could not create entry: %s\n", err.Error())
 			}
 		}
 	}
+	return nil
+}
 
+func main() {
+	err := run()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
